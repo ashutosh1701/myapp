bd9589f42304a1d06c75d055ee60b6faf2af1184
Ubuntu <you@example.com>
2024-10-06 08:29:04 +0000

    Automated commit: Updated files

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, This is OpenShift Admin-Ashutosh adhikari!, too cool'
+    return 'Hello, This is OpenShift Admin-Ashutosh adhikari!, too cool ...'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
